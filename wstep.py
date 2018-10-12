9ae1118060c557f6d34caf7d5a27370a8fa53a52
jakis tam mail <andrzej.maruda@wp.pl>
2018-10-12 17:59:00 +0200

    Dodano wstep.py

---
+++ wstep.py
@@ -0,0 +1,34 @@
+#To je komentarz tego komputjer nie czyta :D
+#wielo linijkowy haks
+"""
+Stachu ma raka
+"""
+#Podstawowe typy danych w pythonie :)
+#String's czyli ciągi znaków
+
+owca0 = "Stachu ma raka"
+owca1 = 'Stachu ma dwa raki i jedne zapalenie płuc'
+#łonczenie napisuf
+"napis" + "kolejny napis"
+#łączenie napisów z liczbami
+"1" + 7 #Error
+"1" + str(7) #Był sobie error wynikiem bedzie 17
+#wyświetlanie do konsoli
+print ( "napis" )
+print(owca)
+print(owca owca "napis napis" 19)
+print(f'')``
+print(f'Stachu ma raka, {436289} {owca1}')
+#wyciaganie kawałków napisów
+#wyciaganie kawałków napisów od indeksu 2 do 4
+print(owca[2:5])
+# Liczby mają dwa typu Intiger i Float
+owcaI = 7 # intiger -> całkowita
+owcaF = 7,5 # float -> rzeczywista
+#Ciekawe polecenia na liczbach
+2**5 # 32 bo to jest 2 do 5
+2e2 # 2 * (10 do 2)
+2e-1 # 2 *(10 do -1) -> 2/10 -> 1/5
+# Boolean Prawda fałsz
+owcaT = prawda
+owcaF = fałsz
